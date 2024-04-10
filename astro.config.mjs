d6bd97b7a2fe5f895d366d24417eea92d42cc6da
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-04-10 23:02:02 +0800

    update dependencies
    enable cloudflare SSG, replace dayjs to timeago.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 // noinspection JSUnusedGlobalSymbols
 
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(
-      {
-        include: {
-          "material-symbols": [
-              "search",
-              "person-edit-outline-rounded",
-              "calendar-today-outline-rounded",
-              "edit-calendar-outline-rounded"
-          ]
-        }
-      }
-  )]
+  output: "hybrid",
+  integrations: [icon({
+    include: {
+      "material-symbols": ["search", "person-edit-outline-rounded", "calendar-today-outline-rounded", "edit-calendar-outline-rounded"]
+    }
+  })],
+  adapter: cloudflare()
 });
