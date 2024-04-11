e72c7017c6e78bf65fab49460f2d89fcd1e11bd5
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-04-11 06:59:03 +0000

    add postcss plugins +autoprefixer, postcss-preset-env, cssnano

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: [
+        require("autoprefixer"),
+        require("postcss-preset-env"),
+        require("cssnano")({
+            preset: "default",
+        }),
+    ],
+};
