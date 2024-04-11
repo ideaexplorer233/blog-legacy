516dfce7e8b1ec2286b5f919cac9f25471cda2c8
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-04-11 07:07:54 +0000

    fix ts warnings

--- src/components/meta.ts
+++ src/components/meta.ts
@@ -1,14 +1,14 @@
 // noinspection SpellCheckingInspection
 
 /*
-* Data from:
-* https://developers.facebook.com/docs/javascript/internationalization
-* https://developers.facebook.com/docs/messenger-platform/messenger-profile/supported-locales
-* https://www.w3.org/International/questions/qa-choosing-language-tags
-*/
+ * Data from:
+ * https://developers.facebook.com/docs/javascript/internationalization
+ * https://developers.facebook.com/docs/messenger-platform/messenger-profile/supported-locales
+ * https://www.w3.org/International/questions/qa-choosing-language-tags
+ */
 
 const tags: any = {
-    "en": "en_US", // IANA don't have a UK variation
+    en: "en_US", // IANA don't have a UK variation
     "zh-Hans-CN": "zh_CN",
     "zh-Hans-HK": "zh_HK",
     "zh-Hans-TW": "zh_TW",
@@ -16,9 +16,9 @@ const tags: any = {
     "zh-Hant-HK": "zh_HK",
     "zh-Hant-TW": "zh_TW",
     // Add more if necessary
-}
+};
 
-export function getMetaLangTag(tag: string): string {
+export function getMetaLangTag(tag?: string): string {
     // Translate IANA language tags to Meta specification
     return tag ? tags[tag] : "en_US";
 }
