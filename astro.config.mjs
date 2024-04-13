41d850c1eb2c902f02299022abdf0a0e39d775e5
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-04-13 15:54:05 +0000

    feat: enable rss, disable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,36 @@
 // noinspection JSUnusedGlobalSymbols
 
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  integrations: [icon({
-    include: {
-      "material-symbols": ["search", "person-edit-outline-rounded", "calendar-today-outline-rounded", "edit-calendar-outline-rounded"]
-    }
-  })],
-  adapter: cloudflare()
+    // output: "hybrid",
+    // adapter: cloudflare(),
+    integrations: [
+        icon({
+            include: {
+                "material-symbols": [
+                    "search",
+                    "person-edit-outline-rounded",
+                    "calendar-today-outline-rounded",
+                    "edit-calendar-outline-rounded",
+                ],
+            },
+        }),
+    ],
+    markdown: {
+        shikiConfig: {
+            theme: "material-theme-ocean",
+        },
+    },
+    image: {
+        service: {
+            entrypoint: "astro/assets/services/sharp",
+            config: {},
+        },
+    },
+    site: "https://example.com", // TODO:change in product version
 });
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+// TODO: Complete this feature
+
+export async function GET(context: any) {
+    const posts = await getCollection("posts");
+    return rss({
+        title: "Astro Learner | Blog",
+        description: "My journey learning Astro",
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.slug}/`,
+        })),
+        customData: `<language>en-us</language>`,
+    });
+}
