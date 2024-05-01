3d57e977958fc049428bf087d7341f177bf1c0e1
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-05-01 20:42:20 +0800

    feat: robots.txt, rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,22 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import { config } from "../config.ts";
 
 // TODO: Complete this feature
 
 export async function GET(context: any) {
     const posts = await getCollection("posts");
     return rss({
-        title: "Astro Learner | Blog",
-        description: "My journey learning Astro",
+        title: config.siteName,
+        description: config.siteDescription,
         site: context.site,
         items: posts.map((post) => ({
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            link: `/posts/${post.slug}/`,
+            link: `/post/${post.slug}/`,
         })),
+        // TODO: we should have i18n here (create one in zh dir)
         customData: `<language>en-us</language>`,
     });
 }
